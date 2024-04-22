c084d91693e2b1c564fa3962474089191605aefc
Aztenz <aztenz@outlook.com>
2024-04-22 23:08:54 +0200

    Initial commit

---
+++ back_end/src/test/java/com/j2o/sentinel/SentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j2o.sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
